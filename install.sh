50b231d4de1d7b22ee0ff2dc13299293d46ba20b
Gradrien <adrien.ricou@epitech.eu>
2023-03-13 16:30:46 +0100

    [FIX] :bug: --> Fixed + optimized installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pdf_pkg='enscript'
-gui_pkg='zenity'
+enscript_pkg='enscript'
+zenity_pkg='zenity'
 
 # Find the current os
 if [ -f /etc/os-release ]; then
@@ -34,11 +34,11 @@ else
 fi
 
 # Select the package manager
-if [ $OS = "Debian" ] || [ $OS = "Ubuntu" ] || [ $OS = "Linux Mint" ]
+if [ $OS = "Debian" ] || [ $OS = "Ubuntu" ] || [ $OS = "Linux Mint" ]; then
   PM='apt-get'
-else if [ $OS = "Fedora" ] || [ $OS = "CentOS" ] || [ $OS = "RHEL" ]
+elif [ $OS = "Fedora" ] || [ $OS = "CentOS" ] || [ $OS = "RHEL" ]; then
   PM='dnf'
-else if [ $OS = "Arch Linux" ]
+elif [ $OS = "Arch Linux" ]; then
   PM='pacman'
 else
   echo "gitsc: Can't find a package manager"
@@ -46,7 +46,10 @@ else
 fi
 
 #Clone the project to home
-git clone git@github.com:Gradrien/Git-Shortcuts.git $HOME/Git-Shortcuts
+if ! curl -fsSL https://github.com/Gradrien/Git-Shortcuts/archive/master.tar.gz | tar -xz --strip-components=1 -C $HOME/Git-Shortcuts; then
+    echo "Failed to clone the Git-Shortcuts project"
+    exit 1
+fi
 sleep 1
 echo "Creating link with zsh"
 sed -i '/export PATH=/ s/$/$HOME\/\Git-Shortcuts:/' ~/.zshrc
@@ -54,35 +57,31 @@ sleep 1
 echo "Link created"
 #Insatllation
 sleep 1
-echo "Installing manual entry"
+echo -e "\033[32mInstalling manual entry\033[0m"
 sudo cp githelp /usr/share/man/man1/gitsc.1
 sudo gzip /usr/share/man/man1/gitsc.1
-echo "Checking your packages"
+echo -e "\033[32mChecking your packages\033[0m"
+
 sleep 1
 
 # Check the different packages
-if rpm -q $pdf_pkg
-then
-  echo "Package installed"
-else
-  read -p "Do you want to install zenity ? [Yy] or [Nn] : " yn
-  case $yn in
-        [Yy]* ) echo "Installing package: $pdf_pkg";
-                sudo $PM install $pdf_pkg; exit;;
-        [Nn]* ) echo "Package not installed"; exit;;
-        * ) echo "Package not installed"; exit;;
-  esac
-fi
+install_package() {
+    local pkg=$1
+    if rpm -q "$pkg"; then
+        echo "Package $pkg is already installed"
+    else
+        read -p "Do you want to install $pkg? [Yy] or [Nn] : " yn
+        case $yn in
+            [Yy]* )
+                echo "Installing package: $pkg"
+                sudo "$PM" install "$pkg"
+                ;;
+            *)
+                echo "Package not installed"
+                ;;
+        esac
+    fi
+}
 
-if rpm -q $gui_pkg
-then
-  echo "Package installed"
-else
-  read -p "Do you want to install zenity ? [Yy] or [Nn] : " yn
-  case $yn in
-        [Yy]* ) echo "Installing package: $gui_pkg";
-                sudo $PM install $gui_pkg; exit;;
-        [Nn]* ) echo "Package not installed"; exit;;
-        * ) echo "Package not installed"; exit;;
-  esac
-fi
+install_package "$enscript_pkg"
+install_package "$zenity_pkg"
